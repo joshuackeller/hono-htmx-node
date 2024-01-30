8c117cb2efbd5e1d0ffdb7487b53169646ad7538
joshuackeller <joshuackeller@gmail.com>
2024-01-30 00:54:16 -0700

    remove prisma

--- prisma/migrations/20240130055753_stuff/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "todo" (
-    "id" VARCHAR NOT NULL,
-    "name" VARCHAR NOT NULL,
-    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "todo_pkey" PRIMARY KEY ("id")
-);
